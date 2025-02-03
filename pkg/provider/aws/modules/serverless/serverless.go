a1925eb44456179e33a6099f2cbbfad4864025cd
Adrian Riobo <ariobolo@redhat.com>
2025-02-03 12:33:32 +0100

    chore: minor fixes around mac pool service serverless house keeping during destroy operations
    Signed-off-by: Adrian Riobo <ariobolo@redhat.com>

--- pkg/provider/aws/modules/serverless/serverless.go
+++ pkg/provider/aws/modules/serverless/serverless.go
@@ -80,6 +80,13 @@ func (a *serverlessRequestArgs) deploy(ctx *pulumi.Context) error {
 	if err != nil {
 		return err
 	}
+	lga := &awsx.DefaultLogGroupArgs{
+		Args: &awsx.LogGroupArgs{
+			SkipDestroy:     pulumi.Bool(true),
+			RetentionInDays: pulumi.Int(3)}}
+	if len(a.logGroupName) > 0 {
+		lga.Args.Name = pulumi.String(a.logGroupName)
+	}
 	td, err := awsxecs.NewFargateTaskDefinition(ctx,
 		resourcesUtil.GetResourceName(a.prefix, a.componentID, "fg-task"),
 		&awsxecs.FargateTaskDefinitionArgs{
@@ -97,12 +104,7 @@ func (a *serverlessRequestArgs) deploy(ctx *pulumi.Context) error {
 			TaskRole: &awsx.DefaultRoleWithPolicyArgs{
 				RoleArn: roleArn,
 			},
-			LogGroup: &awsx.DefaultLogGroupArgs{
-				Args: &awsx.LogGroupArgs{
-					SkipDestroy:     pulumi.Bool(true),
-					RetentionInDays: pulumi.Int(3),
-				},
-			},
+			LogGroup: lga,
 		})
 	if err != nil {
 		return err
